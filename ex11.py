7cb870c7445280e6740b8eae5d920e5e9909d132
CJ <43323243+cdwhitejr@users.noreply.github.com>
2020-10-21 18:03:39 +0100

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -3,3 +3,29 @@
 '''
 Create a Portscanner with sockets with sockets use the gethostbyname
 and the gethostbyaddr in your code
+'''
+
+import ipaddress
+
+
+x = 1
+iplist = []
+while x <256:
+    iplist.append('192.168.1.'+str(x))
+    x +=1
+
+for ip in iplist:
+    #print(ip)
+    pass
+
+network_add = ''
+subnet_mask = ''
+host_add = ''
+cidr = ''
+
+my_add = ipaddress.ip_address('192.168.1.147')
+my_net = ipaddress.ip_network('192.168.1.0/24')
+print(my_net.num_addresses)
+
+my_list = my_net.subnets()
+print(my_list)
