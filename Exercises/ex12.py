d8d3606269060fe1afcd546e802fe7a93ebc279c
CJ <43323243+cdwhitejr@users.noreply.github.com>
2020-10-22 15:20:01 +0100

    retreived lost edit

--- Exercises/ex12.py
+++ Exercises/ex12.py
@@ -10,26 +10,39 @@ from collections import Counter
 
 def read_packet():
 
+
     #creates empty list 
     srcIP = []
+
+    pktdict = {}
+
     packets = rdpcap('.\HTTP_traffic.pcap')
-    p_set = set()
+
+    x = 0
     for pkt in packets:
         if IP in pkt:
             try:
                 srcIP.append(pkt[IP].src)
+                pktdict[x] = {'src_ip':pkt[IP].src, 'dst_ip':pkt[IP].dst,'src_port':pkt[TCP].sport,'dest_port':pkt[TCP].dport}
             except:
                 pass
+        x +=1
     
-    #Create counter
-    count = Counter()
-    for ip in srcIP:
-        count[ip] +=1
-
     #create table
     table = PrettyTable(['Src IP','Dest IP','Count'])
     for ip, count in count.most_common():
         table.add_row([ip, count])
+    table = PrettyTable(['Src IP', 'Src Port','Dest IP', 'Dest Port'])
+    for k,v in pktdict.items():
+        
+        table.add_row([pktdict[k]['src_ip'], pktdict[k]['dst_ip'], pktdict[k]['src_port'], pktdict[k]['dest_port']])
+    
+
+    table_text = table.get_string()
+
+    with open('extracted_data.txt','w') as f:
+        f.writelines(table_text)
+           
     
     print(table)
 read_packet()
