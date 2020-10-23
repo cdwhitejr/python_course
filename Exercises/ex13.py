4dae99a7842999d09f1a7e74a7d00151b83550da
CJ <43323243+cdwhitejr@users.noreply.github.com>
2020-10-23 14:24:07 +0100

    new ex

---
+++ Exercises/ex13.py
@@ -0,0 +1,10 @@
+#ex13.py
+
+'''
+Create a small python code, importing the Yara module and compre the rule we already made
+Create against the Lab malware provided (In kali)
+Print the results of the comparison
+'''
+
+import yara
+
