81766605d65d5fd1cd25b6179edb90d08f4c516a
root <root@pandora.pandora.local>
2020-10-23 08:46:28 -0500

    synce with linux

--- Exercises/ex13.py
+++ Exercises/ex13.py
@@ -5,6 +5,11 @@ Create a small python code, importing the Yara module and compre the rule we alr
 Create against the Lab malware provided (In kali)
 Print the results of the comparison
 '''
-
+import os
 import yara
 
+rules = yara.compile(filepath='yara_rule.yar')
+
+matches = rules.match('/root/Desktop/Malware/Lab01-02.exe')
+
+print(matches[0].strings)
