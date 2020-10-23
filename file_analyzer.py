c7386cb852a2ac7a5f3a651ad07b983b6657ebdf
root <root@pandora.pandora.local>
2020-10-23 09:19:16 -0500

    update

--- file_analyzer.py
+++ file_analyzer.py
@@ -3,9 +3,9 @@
 
 import pefile
 
-path = '/root/Desktop/Malware/Lab01-02.ext'
+path = '/root/Desktop/Malware/Lab01-02.exe'
 
 pe = pefile.PE(path)
 
-print(f'Magic number is: {hex(pe.DOS_HEADER.e_magic)')
-print(f'')
+print(f'Magic number is: {hex(pe.DOS_HEADER.e_magic)}')
+print(f'Signature is: {hex(pe.NT_HEADERS.Signature)}')
